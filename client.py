0433490d78fb374aece963ef30984c85e4021f53
Noah Hallows <noah@quackmail.com.au>
2025-05-10 10:45:48 +1000

    added inital login system

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import bcrypt
 
 HOST = "127.0.0.1"
 PORT = 28752
@@ -21,12 +22,39 @@ def recive_message(s):
             break
         print(f"Recived {data.decode()}")
 
+def login(s):
+    while True:
+        username = input("Enter your username: ")
+        if username == ":q":
+            s.sendall("\0".encode())
+            break
+        password = input("Enter your password: ")
+        s.sendall(username.encode())
+        data = s.recv(1024)
+        if data.decode().strip() != "False":
+            salt = data.decode().strip()
+            password_hash = bcrypt.hashpw(password.encode(), salt.encode())
+            s.sendall(password_hash)
+            data = s.recv(1024)
+            logined = data.strip()
+            if logined == b'1':
+                print("Successful login")
+                return username, password_hash
+            else:
+                print("Incorrect username or password")
+                salt = None
+                continue
+        else:
+            print("Incorrect username or password")
+            salt = None
+            continue
+
+
 def main():
     global shutdown
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    username = input("Enter your username: ")
-    s.sendall(username.encode())
+    username, password_hash = login(s)
     send_message_thread = threading.Thread(target=send_message, args=(s, ))
     recive_message_thread = threading.Thread(target=recive_message, args=(s, ), daemon=True)
     send_message_thread.start()
